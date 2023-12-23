5d72dc1fab17580606aeb8cfad468e250fd51288
Anton Klock <klock.anton@gmail.com>
2023-12-23 22:51:01 +0100

    adding credentials to socket io

--- socket.js
+++ socket.js
@@ -5,4 +5,4 @@ const herokuURL = "https://dome-concert-controller-server-180a81f5a181.herokuapp
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = process.env.NODE_ENV === 'production' ? herokuURL : 'http://localhost:4010';
 
-export const socket = io(URL);
+export const socket = io(URL, { withCredentials: true });
