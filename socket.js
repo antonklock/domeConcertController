35527141b15d953de44c03a99a09099f554c74e8
Anton Klock <klock.anton@gmail.com>
2023-12-23 22:35:10 +0100

    updating test url for heroku

--- socket.js
+++ socket.js
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client';
 
+const herokuURL = "https://dome-concert-controller-server-180a81f5a181.herokuapp.com/";
+
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:4010';
+const URL = process.env.NODE_ENV === 'production' ? herokuURL : 'http://localhost:4010';
 
 export const socket = io(URL);
