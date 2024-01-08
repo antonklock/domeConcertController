8be79cb87c7d9be84858f2ab15296bc388de7eb3
Anton Klock <klock.anton@gmail.com>
2024-01-08 21:47:56 +0100

    preparing for merge with main

--- socket.js
+++ socket.js
@@ -1,10 +1,10 @@
 import { io } from 'socket.io-client';
 
 //TODO: MOVE TO ENV FILE
-const herokuURL = "wss://dome-concert-controller-server-180a81f5a181.herokuapp.com";
+const herokuURL = "https://dome-concert-controller-server-180a81f5a181.herokuapp.com";
 
 
-const URL = process.env.NODE_ENV === 'production' ? herokuURL : 'ws://localhost:3010';
+const URL = process.env.NODE_ENV === 'production' ? herokuURL : 'http://localhost:3010';
 
 
 export const socket = io(URL, { withCredentials: true, autoConnect: false });
