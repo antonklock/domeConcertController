d7a6f8ea547513b22dc5b77c78c0681d8a9452d6
Anton Klock <klock.anton@gmail.com>
2024-01-08 21:43:41 +0100

    Trying to add use wss protocal for websockets

--- socket.js
+++ socket.js
@@ -1,10 +1,10 @@
 import { io } from 'socket.io-client';
 
 //TODO: MOVE TO ENV FILE
-const herokuURL = "https://dome-concert-controller-server-180a81f5a181.herokuapp.com";
+const herokuURL = "wss://dome-concert-controller-server-180a81f5a181.herokuapp.com";
 
 
-const URL = process.env.NODE_ENV === 'production' ? herokuURL : 'http://localhost:3010';
+const URL = process.env.NODE_ENV === 'production' ? herokuURL : 'ws://localhost:3010';
 
-//TODO: Remove withCredentials after testing??
+
 export const socket = io(URL, { withCredentials: true, autoConnect: false });
